ab9e4972fab59a9ac69d635e2ccf242b5cc03dd3
eivindsd <eivindsd@stud.ntnu.no>
2021-03-17 16:42:01 +0100

    implement internal shell commands

--- PE4/wish.c
+++ PE4/wish.c
@@ -49,8 +49,8 @@
 int main() {
     while(69){
         //char** tokens = tokenize();
-        char terminal_line[30];
-        static char * tokens[20];
+        char terminal_line[100];
+        static char * tokens[100];
         char* input;
         char* output;
         int crocodile=1;
@@ -70,7 +70,7 @@ int main() {
             int n = 1;
             int i = 1;
             while ((strcmp(tokens[n], "<")!=0) && (strcmp(tokens[n], ">")!=0)) {
-                printf(" %s", tokens[n]);
+                printf("%s", tokens[n]);
                 n++;
                 i = n;
                 crocodile = n;
@@ -92,10 +92,19 @@ int main() {
         printf("\n");
         }
         tokens[m] = NULL;
+
+        if(strcmp(tokens[0], "cd")==0) {
+            chdir(tokens[1]);
+            continue;
+        }
+        else if(strcmp(tokens[0], "exit")==0) {
+            exit(0);
+        }
+
         int pid = fork();
         pid_t wpid;
         int status;
-      
+
         if (pid == 0 ) {
             if(output) {
                 int fd_out = creat(output, 0644);
@@ -109,7 +118,7 @@ int main() {
             }
             
             if (execvp(tokens[0], tokens) == -1) {
-                perror("Error"); 
+                perror("Errors"); 
             } 
         } 
         else if(pid > 0) {
